aa5d410a73c4dc3d34c6bf571b2c51fbbf639529
RYAN <126591328+ryanmarcelos@users.noreply.github.com>
2025-08-07 09:52:33 -0300

    Add files via upload

---
+++ Variaveis.java
@@ -0,0 +1,20 @@
+public class Variaveis{
+    public static void main(String[] args){
+
+        //tipo nome = conteudo;
+
+        String meuNome = "Ryan";
+        meuNome += " Silva";
+        int idade = 17;
+        double altura = 1.75;
+        char sexo = 'M';
+        boolean casado = false;
+
+
+        System.out.print("Meu nome é "+meuNome);
+        System.out.print(" e minha idade é "+idade);
+        System.out.print("anos e minha altura é "+altura);
+        
+
+    }
+}
